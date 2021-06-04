c26ea14ff394f3795e10d7799a4bdfdbe775654e
Aaron Gabriel <aarongabriel147@gmail.com>
2021-06-04 15:01:49 -0700

    Initial set up complete.

---
+++ team-builder/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './components/Form'
+import React, { useState } from 'react';
+
+export default function App() {
+
+console.log('I like tacos!')
+
+  return (
+    <div className="test">
+      <p>This is a test!</p>
+    </div>
+  );
+};
+
+
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Form() {
+
+
+  return (
+    <form>
+        <label>
+            <input />
+        </label>
+    </form>
+  );
+};
+
+
