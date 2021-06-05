440595b914e768533fa77ce1cbeb9c02bcc8188d
Aaron <aarongabriel147@gmail.com>
2021-06-05 14:24:18 -0700

    finished

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,16 +1,61 @@
-import './App.css';
-import Form from './components/Form'
 import React, { useState } from 'react';
+import Form from './components/Form'
+import './App.css';
+// {/* */}
+
+const initialValues = { name:"", email:"", role:"" };
+
+// const teamMembers = [
+//   {
+//     name:"Tim",
+//     email:"timmythetooth@gmail.com",
+//     role:"Conspiracy theorist."
+//   },
+//   {
+//     name:"Dave",
+//     email:"dtoon@gmail.com",
+//     role:"Musician"
+//   },
+// ];
+
 
 export default function App() {
 
-console.log('I like tacos!')
+  const [members, setMembers] = useState(teamMembers);                   // {name:"Tim", email:"timmythetooth@gmail.com", ...
+  const [formValue, setFormValue] = useState(initialValues);             // { name:"", email:"", role:"" };
+
+  console.log('_CONSOLE TEST_')
+
+  function updateBananaApp(inputNameBanana, inputValueBanana) {
+    setFormValue({ ...formValue, [inputNameBanana]: inputValueBanana })
+  }
+
+  function submitBananaApp() {
+    const newMember = {
+      name: formValue.name,
+      email: formValue.email
+    }
+    setMembers([newMember, ...members])      // '...members' makes it so it saves the previous members. Needs to be an arr so .map works.
+    setFormValue(initialValues)                               
+  }
 
   return (
     <div className="test">
-      <p>This is a test!</p>
+
+      {/* {members.map(member => (
+        <div>
+          {member.name}
+          </div>
+      ))} */}
+
+      <Form 
+      valuesBanana={formValue} 
+      updateBanana={updateBananaApp} 
+      submitBanana={submitBananaApp} 
+      />                                                 {/* Passing state to the form component. */}
+
     </div>
   );
-};
+}
 
 
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Form() {
+
+export default function Form(props) {
+  
+
+  const changeHandlerBanana = (e) => {
+    const inputNameBanana = e.target.name;           {/* takes var name from input */}
+    const inputValueBanana = e.target.value;         {/* takes var value from input */}
+    props.updateBanana(inputNameBanana, inputValueBanana)
+  }
+
+  const submitHandlerBanana = (e) => {
+    e.preventDefault();
+    props.submitBanana();
+  }
 
 
   return (
-    <form>
+    <form onSubmit={submitHandlerBanana}>            {/* onSubmit is a keyword */}
         <label>
-            <input />
+
+            <input 
+              type="text" 
+              name="name" 
+              value={props.valuesBanana.name} 
+              onChange={changeHandlerBanana}
+            />
+
+            <input 
+              type="text" 
+              name="email" 
+              value={props.valuesBanana.email} 
+              onChange={changeHandlerBanana}
+            />
+
         </label>
+
+    <button>CLICK</button>
     </form>
   );
 };
-
-
